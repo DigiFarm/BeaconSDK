041b52b357147ce773713e9e94c8aa268a2b941b
Paul Himes <paul@paulhimes.com>
2016-07-21 00:54:30 -0500

    Connect BeaconSDK pod to example project.

--- Example/Pods/Target Support Files/BeaconSDK/BeaconSDK-umbrella.h
+++ Example/Pods/Target Support Files/BeaconSDK/BeaconSDK-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "BeaconSDK.h"
+#import "GCDAsyncSocket.h"
 
 FOUNDATION_EXPORT double BeaconSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char BeaconSDKVersionString[];
